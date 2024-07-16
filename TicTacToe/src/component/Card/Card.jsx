704dc662e936f53b5d3e8d2c1975d9f7ef90c200
Kundan <kk9797307@gmail.com>
2024-07-16 11:20:49 +0530

    Finalize the game

--- TicTacToe/src/component/Card/Card.jsx
+++ TicTacToe/src/component/Card/Card.jsx
@@ -1,15 +1,19 @@
 import Icon from "../Icon/Icon";
 import './Card.css';
 
-function Card({iconName, onPlay}){
-    function playMove() {
-        onPlay();
+function Card({ onPlay, player, gameEnd, index}){
+    let icon = <Icon/>
+    if(player == "X"){
+        icon = <Icon name={"cross"} />
+    }else if(player == "O"){
+        icon = <Icon name={"circle"} />
     }
     return(
-        <div className="card" onClick={playMove} >
-            <Icon name={iconName} />
+        <div className="card"  onClick={()=> !gameEnd && player== "" && onPlay(index)} >
+            {icon}
         </div>
     )
+
 }
 
 export default Card;
--- TicTacToe/src/component/Grid/Grid.jsx
+++ TicTacToe/src/component/Grid/Grid.jsx
@@ -1,22 +1,53 @@
 import { useState } from "react";
 import Card from "../Card/Card";
+import isWinner from "../helper/CheckWinner";
+import { ToastContainer, toast } from 'react-toastify';
+
 import './Grid.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Grid({numberOfCards}) {
     const [ turn, setTurn ] = useState(true); // if true-> 0, flase->x
-    function Play(){
-        console.log("move turn");
-        setTurn(!turn)
+    const [ board, setBoard ] = useState(Array(numberOfCards).fill("")); // [" "," "," "," "," "," "," "," "," "]
+    const [ winner, setWinner ] = useState(null);
+
+    function Play(index){
+        if(turn == true) {
+            board[index] = "O";
+        } else {
+            board[index] = "X";
+        }
+        let win = isWinner(board, turn ? "O" : "X");
+        if(win) {
+            setWinner(win);
+            toast.success(`Congratulation ${win} win the game`);
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
+
+    function reset() {
+        setBoard(Array(numberOfCards).fill(""));
+        setWinner(null);
+        setTurn(true);
     }
     return(
-        <>
+        <div className="gridWrapper">
+            {winner && (
+                <>
+                    <h1 className="win">Winner is {winner}</h1>
+                    <button className="reset" onClick={reset}>Reset Game</button>
+                    <ToastContainer position="top-center" />
+                </>
+            )}
             <h1 className="turn-heighlight">Current turn: {(turn)? 'O': 'X' }</h1>
             <div className="grid">
-                {Array(9).fill(<Card />).map((el, idx)=>{
-                    return <Card onPlay={Play} key={idx}/>
+                {board.map((value, idx)=>{
+                    return <Card gameEnd= {winner? true: false} onPlay={Play} player={value} key={idx} index={idx}/>
                 })}
             </div>
-        </>
+        </div>
     )
 }
 
